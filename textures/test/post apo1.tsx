562beb7cf645a634bd3505b49f904db0d722f8c5
PaulSup06 <paulsup2006@gmail.com>
2023-03-04 00:20:25 +0100

    début de map ?

---
+++ textures/test/post apo1.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.8" tiledversion="1.8.0" name="post apo1" tilewidth="64" tileheight="64" tilecount="1040" columns="40">
+ <image source="post apo1.png" width="2560" height="1664"/>
+</tileset>
