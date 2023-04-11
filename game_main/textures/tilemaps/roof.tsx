6dcad148556f14576b5739580a10cc4ca9168130
zonetecde <zonetecde@gmail.com>
2023-04-11 21:27:37 +0200

    map avancement

---
+++ game_main/textures/tilemaps/roof.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.10.1" name="roof" tilewidth="64" tileheight="64" tilecount="168" columns="12">
+ <image source="roof.png" width="768" height="896"/>
+</tileset>
---
+++ game_main/textures/tilemaps/tf_A5_ashlands_3.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.10.1" name="tf_A5_ashlands_3" tilewidth="64" tileheight="64" tilecount="121" columns="11">
+ <image source="tf_A5_ashlands_3.png" width="704" height="704"/>
+</tileset>
