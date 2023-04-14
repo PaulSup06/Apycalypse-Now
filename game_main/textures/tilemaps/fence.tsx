187c6483a186e37f3326c3bd220250d7c0821bfc
zonetecde <zonetecde@gmail.com>
2023-04-14 22:46:16 +0200

    avancement map

---
+++ game_main/textures/tilemaps/fence.tsx
@@ -0,0 +1,28 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.9" tiledversion="1.9.2" name="fence" tilewidth="64" tileheight="64" tilecount="320" columns="40">
+ <image source="fence.png" width="2560" height="512"/>
+ <wangsets>
+  <wangset name="fence_black" type="edge" tile="-1">
+   <wangcolor name="" color="#ff0000" tile="-1" probability="1"/>
+   <wangtile tileid="155" wangid="0,0,1,0,1,0,0,0"/>
+   <wangtile tileid="156" wangid="0,0,1,0,0,0,1,0"/>
+   <wangtile tileid="157" wangid="0,0,0,0,1,0,1,0"/>
+   <wangtile tileid="158" wangid="0,0,0,0,1,0,0,0"/>
+   <wangtile tileid="195" wangid="0,0,1,0,1,0,0,0"/>
+   <wangtile tileid="196" wangid="0,0,1,0,0,0,1,0"/>
+   <wangtile tileid="197" wangid="0,0,0,0,1,0,1,0"/>
+   <wangtile tileid="198" wangid="0,0,0,0,1,0,0,0"/>
+   <wangtile tileid="235" wangid="1,0,0,0,1,0,0,0"/>
+   <wangtile tileid="237" wangid="1,0,0,0,1,0,0,0"/>
+   <wangtile tileid="238" wangid="1,0,0,0,1,0,0,0"/>
+   <wangtile tileid="275" wangid="1,0,1,0,1,0,0,0"/>
+   <wangtile tileid="276" wangid="0,0,1,0,0,0,1,0"/>
+   <wangtile tileid="277" wangid="1,0,0,0,1,0,1,0"/>
+   <wangtile tileid="278" wangid="1,0,0,0,1,0,0,0"/>
+   <wangtile tileid="315" wangid="1,0,1,0,1,0,0,0"/>
+   <wangtile tileid="316" wangid="1,0,1,0,0,0,1,0"/>
+   <wangtile tileid="317" wangid="0,0,1,0,1,0,1,0"/>
+   <wangtile tileid="318" wangid="1,0,0,0,1,0,1,0"/>
+  </wangset>
+ </wangsets>
+</tileset>
